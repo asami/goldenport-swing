84098d5a318b2d66b64b7260f3bec5bb69c07761
ASAMI, Tomoharu <asami.tomoharu@gmail.com>
2012-02-16 18:31:18 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,26 @@
+organization := "org.goldenport"
+
+name := "goldenport-swing"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.9.1"
+
+scalacOptions += "-deprecation"
+
+scalacOptions += "-unchecked"
+
+resolvers += "Asami Maven Repository" at "http://www.asamioffice.com/maven"
+
+libraryDependencies += "org.scala-lang" % "scala-swing" % "2.9.1"
+
+libraryDependencies += "org.scalaz" %% "scalaz-core" % "6.0.3"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.8" % "test"
+
+libraryDependencies += "org.goldenport" %% "scalatestlib" % "0.1.0" % "test"
+
+//
+publishTo := Some(Resolver.file("asamioffice", file("target/maven-repository")))
---
+++ src/main/scala/org/goldenport/swing/PropertySheetPanel.scala
@@ -0,0 +1,116 @@
+package org.goldenport.swing
+
+import scalaz._
+import Scalaz._
+import scala.swing._
+import org.goldenport.util.ArrayMap
+import org.goldenport.record._
+import scala.swing.event.KeyPressed
+import scala.swing.event.Key
+import java.util.concurrent.CopyOnWriteArrayList
+
+/**
+ * @since   Feb. 16, 2012
+ * @version Feb. 16, 2012
+ * @author  ASAMI, Tomoharu
+ */
+class PropertySheetPanel(val contract: RecordSchema) extends GridBagPanel() {
+  private val _input_fields = new ArrayMap[String, Component]
+  private val _message_fields = new ArrayMap[String, Label]
+  private val _handlers = new CopyOnWriteArrayList[() => Unit]
+
+  var y = 0
+  for (f <- contract.fields) {
+    val cl = new Constraints {
+      gridx = 0
+      gridy = y
+    }
+    val ci = new Constraints {
+      gridx = 1
+      gridy = y
+    }
+    val cm = new Constraints {
+      gridx = 0
+      gridy = y + 1
+      gridwidth = 2
+    }
+    layout += _create_label(f) -> cl
+    layout += _create_input_field(f) -> ci
+    layout += _create_message_field(f) -> cm
+    y += 2
+  }
+
+  private def _create_label(rf: RecordField) = {
+    new Label(rf.name) {
+      tooltip = rf.name // summary
+    }
+  }
+  
+  private def _create_input_field(rf: RecordField) = {
+    val tf = new TextField() {
+      listenTo(keys)
+      reactions += {
+        case KeyPressed(_, Key.Enter, _, _) => _validate(rf, text)
+      }
+    }    
+    _input_fields += rf.name -> tf
+    tf
+  }
+
+  private def _create_message_field(rf: RecordField) = {
+    val mf = new Label("message")
+    _message_fields += rf.name -> mf
+    mf.visible = false
+    mf
+  }
+
+  private def _validate(rf: RecordField, text: String) = {
+    if (_is_valid(rf, text)) {
+      _message_clear(rf.name)
+    } else {
+      _message_warning(rf.name, "XXX")
+    }
+  }
+
+  private def _is_valid(rf: RecordField, text: String) = {
+    false
+  }
+
+  private def _message_clear(name: String) {
+    _message_fields.get(name).foreach(_.visible = false)
+  }
+
+  private def _message_warning(name: String, message: String) {
+    _message_fields.get(name).foreach { v =>
+      v.text = message
+      v.visible = true
+    }
+  }
+
+  // XXX
+  def getRecord() = {
+    _input_fields
+  }
+
+  def getPropertyStringList(): List[(String, String)] = {
+    for ((n, c) <- _input_fields.toList) yield {
+      (n, _get_string_value(c))
+    }
+  }
+
+  private def _get_string_value(c: Component): String = {
+    c match {
+      case t: TextField => t.text
+    }
+  }
+
+  def addPropertyChangeHandler(h: () => Unit) {
+    _handlers.add(h)
+  }
+
+  private def _fire() {
+    for (f <- _handlers) {
+      f.apply()
+    }
+  }
+}
